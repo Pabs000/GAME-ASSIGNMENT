5688745aa0bbc3a3283d78befd3704d9a316b37a
Pabs000 <97498808+Pabs000@users.noreply.github.com>
2023-07-08 04:51:04 +0000

    game changes

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-let speed = 10;
+let speed = 7;
 
+let tileCount= 20;
+let tileSize = canvas.width / tileCount - 2;
+let headX = 10;
+let headY = 10;
+
+
+//game loop for drawing GAME
 function drawGame(){
-console.log('draw game')
-    setTimeout(drawGame, 1000/ speed) ;
+    clearScreen();
+    drawSnake();
+    setTimeout(drawGame, 1000/ speed);
 }
+// setTimeOut to adjust game speed(difficulty)
+// 1000 Millaseconds = 1 second 
+
+function clearScreen(){
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(0,0,canvas.width, canvas.height)
+}
+//COLOR OF THE DRAWING BOARD - Fill RECTANGLE where 0,0 is starting point and covers the size of canvas Width and Height
+
+function drawSnake(){
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(headX * tileCount, headY * tileCount, tileSize,tileSize);
+}
+//drawing the snake as a rectangle(head x and y multiplied against tileCount to position in Tiles) 
+
 
 drawGame();
+
